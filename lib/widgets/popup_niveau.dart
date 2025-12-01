b1836144300d7e2d92d3a3a72e3709f0a17f04a6
Cristol Lucie <luciecristol@gmail.com>
2025-12-01 08:31:35 +0100

    popup level files

---
+++ lib/widgets/popup_niveau.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class PopupNiveau extends StatelessWidget {
+  final String popupTitle;
+  final String popupText;
+  const PopupTuto({
+    super.key,
+    required this.popupTitle,
+    required this.popupText,
+  }); // Comme MainApp provient de StatelessWidget on doit la crée avec le super
+
+  
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: const Color(0xFF99B3E7),                 
+        borderRadius: BorderRadius.circular(20)
+      ), 
+      child: Column(
+        children: [
+        Text(popupTitle), 
+        Container(
+          decoration: BoxDecoration(
+            color: const Color(0xFF3950AE),                 
+            borderRadius: BorderRadius.circular(20)
+          ),
+          child: Text(popupText),
+        )
+      ])
+    );
+  }
+}
