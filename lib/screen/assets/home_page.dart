2eafc482c642b9451760cede0471858affd3f9c7
Floriane Louis <Floriane.Louis.Etu@univ-lemans.fr>
2025-12-06 22:41:35 +0100

    Update home page

--- lib/screen/assets/home_page.dart
+++ lib/screen/assets/home_page.dart
@@ -1,9 +1,7 @@
 import 'package:bouquetblossom/constants/app_colors.dart';
-import 'package:bouquetblossom/widgets/app_bar.dart';
-import 'package:bouquetblossom/widgets/bottom_nav_bar.dart';
-import 'package:bouquetblossom/widgets/buttons_home_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:bouquetblossom/widgets/popup_tuto.dart';
 
 class MainPage extends StatefulWidget {
   const MainPage({super.key});
@@ -14,6 +12,7 @@ class MainPage extends StatefulWidget {
 
 class _MainPageState extends State<MainPage> {
   int _selectedIndex = 0;
+  bool _isFirstWaterClick = true; // A mettre dans le local storage plus tard
 
   static const List<String> _titles = <String>['Accueil', 'Collections'];
 
@@ -26,9 +25,14 @@ class _MainPageState extends State<MainPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      // Widget séparé pour l'app bar 
-      appBar: CustomAppBar(
-        title: _titles[_selectedIndex],
+      appBar: AppBar(
+        title: Text(_titles[_selectedIndex]),
+        titleTextStyle: const TextStyle(
+          fontSize: 24,
+          fontWeight: FontWeight.bold,
+        ),
+        toolbarTextStyle: const TextStyle(fontFamily: 'Manrope'),
+        backgroundColor: AppColors.sakuraPink,
       ),
       body: Column(
         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
@@ -53,14 +57,161 @@ class _MainPageState extends State<MainPage> {
               ),
             ],
           ),
-          // Widget séparé pour les boutons
-          const ButtonsHomePage(),
+          Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  ElevatedButton(
+                    onPressed: () {
+                      // Action du bouton --> redirection vers la page de jeu
+                    },
+                    style: ElevatedButton.styleFrom(
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 40,
+                        vertical: 8,
+                      ),
+                      // Couleur de fond du bouton
+                      backgroundColor: AppColors.whitePink,
+                      // Couleur du texte du bouton
+                      foregroundColor: AppColors.sakuraPink,
+                      // Couleur de la bordure du bouton
+                      side: const BorderSide(
+                        color: AppColors.sakuraPink,
+                        width: 2,
+                      ),
+                      fixedSize: const Size(300, 60),
+                    ),
+                    child: const Text(
+                      'Niveau 1',
+                      style: TextStyle(
+                        fontSize: 32,
+                        fontFamily: 'Manrope',
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Column(
+                    children: [
+                      ElevatedButton(
+                        onPressed: () {
+                          // Action du bouton --> ajouter des fleurs au bouquet
+                        },
+                        style: ElevatedButton.styleFrom(
+                          padding: const EdgeInsets.symmetric(
+                            horizontal: 20,
+                            vertical: 8,
+                          ),
+                          textStyle: const TextStyle(
+                            fontSize: 50,
+                            fontFamily: 'Manrope',
+                          ),
+                          backgroundColor: AppColors.matchaGreen,
+                          foregroundColor: AppColors.whitePink,
+                          side: const BorderSide(
+                            color: AppColors.white,
+                            width: 2,
+                          ),
+                          fixedSize: const Size(130, 60),
+                        ),
+                        child: Row(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            const SizedBox(width: 8),
+                            Image.asset(
+                              'assets/images/sakura.webp',
+                              width: 50,
+                              height: 50,
+                            ),
+                            const Text(
+                              '5',
+                              style: TextStyle(
+                                fontSize: 32,
+                                fontFamily: 'Manrope',
+                                fontWeight: FontWeight.bold,
+                                color: AppColors.lightSakuraPink,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                  Column(
+                    children: [
+                      ElevatedButton(
+                        onPressed: () async {
+                          // Action du bouton --> arroser les fleurs du bouquet
+
+                          if (_isFirstWaterClick) {
+                            await showDialog(
+                              context: context,
+                              builder: (context) => const PopupTuto(
+                                popupTitle: "Arrosoir",
+                                popupText:
+                                    "Les fleurs ont besoin d’eau pour pousser, essaie de les arroser !",
+                              ),
+                            );
+                            setState(() {
+                              _isFirstWaterClick = false;
+                            });
+                          }
+                        },
+                        style: ElevatedButton.styleFrom(
+                          padding: const EdgeInsets.symmetric(
+                            horizontal: 20,
+                            vertical: 8,
+                          ),
+                          textStyle: const TextStyle(
+                            fontSize: 50,
+                            fontFamily: 'Manrope',
+                          ),
+                          backgroundColor: AppColors.lightSakuraPink,
+                          foregroundColor: AppColors.whitePink,
+                          side: const BorderSide(
+                            color: AppColors.white,
+                            width: 2,
+                          ),
+                          fixedSize: const Size(130, 60),
+                        ),
+                        child: Row(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            Image.asset(
+                              'assets/images/water.webp',
+                              width: 80,
+                              height: 80,
+                            ),
+                            const SizedBox(width: 8),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ],
+          ),
         ],
       ),
-      // Widget séparé pour la bottom navigation bar
-      bottomNavigationBar: BottomNavBar(
+      bottomNavigationBar: BottomNavigationBar(
         currentIndex: _selectedIndex,
         onTap: _onItemTapped,
+        backgroundColor: AppColors.sakuraPink,
+        selectedItemColor: AppColors.white,
+        unselectedItemColor: AppColors.white,
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Accueil'),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.collections),
+            label: 'Collections',
+          ),
+        ],
       ),
     );
   }
