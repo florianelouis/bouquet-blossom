410b4c03d5484d3944792ad83542bfe97cabf852
Floriane Louis <Floriane.Louis.Etu@univ-lemans.fr>
2025-12-06 22:53:28 +0100

    Comment errors to run project

--- lib/widgets/flower_bloc.dart
+++ lib/widgets/flower_bloc.dart
@@ -1,22 +1,22 @@
-// import 'package:flutter/material.dart';
+// // import 'package:flutter/material.dart';
 
-class FlowerBloc extends StatelessWidget {
-  final String srcImg;
-  final Color color;
-  const FlowerBloc({
-    super.key,
-    required this.srcImg,
-    required this.color,
-  }); // Comme MainApp provient de StatelessWidget on doit la crée avec le super
+// class FlowerBloc extends StatelessWidget {
+//   final String srcImg;
+//   final Color color;
+//   const FlowerBloc({
+//     super.key,
+//     required this.srcImg,
+//     required this.color,
+//   }); // Comme MainApp provient de StatelessWidget on doit la crée avec le super
 
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        color: color,
-        borderRadius: BorderRadius.circular(20),
-      ),
-      child: Image.asset(srcImg),
-    );
-  }
-}
+//   @override
+//   Widget build(BuildContext context) {
+//     return Container(
+//       decoration: BoxDecoration(
+//         color: color,
+//         borderRadius: BorderRadius.circular(20),
+//       ),
+//       child: Image.asset(srcImg),
+//     );
+//   }
+// }
